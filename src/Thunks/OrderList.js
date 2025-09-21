3174563987bf7131f2c2e4d291b3912fc4103bd9
rakeshkumarreddygoddeti <rakeshkumarreddy.fedev@gmail.com>
2025-09-22 00:50:21 +0530

    Fixed API issue.

--- src/Thunks/OrderList.js
+++ src/Thunks/OrderList.js
@@ -3,7 +3,8 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 export const fetchOrderList = createAsyncThunk(
   "orders/fetchOrderList",
   async () => {
-    const response = await fetch("https://juspay-db-json.netlify.app/juspay-db.json");
+    // const response = await fetch("https://juspay-db-json.netlify.app/juspay-db.json");
+    const response = await fetch("/db.json");
     if (!response.ok) {
       throw new Error("Failed to fetch orders");
     }
